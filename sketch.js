a31c0bf29dbc54739320b5b4b6631f3f81815f8f
1baoguai0-0711 <1baoguai0@gmail.com>
2026-05-04 10:32:07 +0800

    2.0

--- sketch.js
+++ sketch.js
@@ -1,4 +1,13 @@
 let capture;
+let faceMesh;
+let faces = [];
+// 指定要串接的臉部特徵點編號
+let faceIndices = [409, 270, 269, 267, 0, 37, 39, 40, 185, 61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291];
+
+function preload() {
+  // 載入 ml5.js 的 FaceMesh 模型
+  faceMesh = ml5.faceMesh();
+}
 
 function setup() {
   // 建立全螢幕畫布
@@ -12,6 +21,9 @@ function setup() {
   
   // 隱藏預設在畫布下方的 HTML 影片元件，只在畫布內繪製
   capture.hide();
+
+  // 開始對攝影機進行臉部偵測
+  faceMesh.detectStart(capture, gotFaces);
 }
 
 function draw() {
@@ -35,10 +47,36 @@ function draw() {
   
   // 繪製影像，注意因為 scale 翻轉了座標系，此處座標設為 (0, 0)
   image(capture, 0, 0, imgW, imgH);
+
+  // 繪製指定的臉部辨識連線
+  if (faces.length > 0) {
+    let face = faces[0];
+    stroke(255, 0, 0); // 設定線條顏色為紅色
+    strokeWeight(15);  // 設定線條粗細為 15
+    noFill();
+
+    // 計算縮放比例，以將偵測到的座標對應到顯示的大小 (50% 螢幕寬高)
+    let sx = imgW / capture.width;
+    let sy = imgH / capture.height;
+
+    // 依序串接點位
+    for (let i = 0; i < faceIndices.length - 1; i++) {
+      let p1 = face.keypoints[faceIndices[i]];
+      let p2 = face.keypoints[faceIndices[i + 1]];
+      
+      if (p1 && p2) {
+        line(p1.x * sx, p1.y * sy, p2.x * sx, p2.y * sy);
+      }
+    }
+  }
   pop();
 }
 
 // 當視窗大小改變時，自動調整畫布大小
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+}
+
+function gotFaces(results) {
+  faces = results;
 }
